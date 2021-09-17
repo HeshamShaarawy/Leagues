32e7346c22a49444b5997a6a12d876c599078712
Hesham <hesham2go@outlook.com>
2021-09-16 23:09:48 -0400

    err

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-//var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var methodOverride = require('method-override');
 
 
-// const session = require('express-session')
-// const passport = require('passport')
+const session = require('express-session')
+const passport = require('passport')
 // load the env vars
-// require('dotenv').config();
+require('dotenv').config();
 
 //connect to the database with Mongoose
 require('./config/database');
@@ -18,9 +18,9 @@ require('./config/database');
 require('./config/passport');
 
 var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-// var courtsRouter = require('./routes/courts');
-// var teamsRouter = require('./routes/teams');
+var usersRouter = require('./routes/users');
+var courtsRouter = require('./routes/courts');
+var teamsRouter = require('./routes/teams');
 var matchesRouter = require('./routes/matches');
 var app = express();
 
@@ -31,31 +31,30 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(cookieParser());
 
-// app.use(session({
-//   secret: 'SEIRocks',
-//   resave: false,
-//   saveUninitialized: true
-// }))
-// app.use(passport.initialize());
-// app.use(passport.session())
+app.use(session({
+   secret: 'SEIRocks',
+   resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session())
 
-// app.use(function (req, res, next) {
-//   res.locals.user = req.user;
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/matches', matchesRouter);
-// app.use('/users', usersRouter);
-// app.use('/teams', teamsRouter);
+app.use('/users', usersRouter);
+app.use('/teams', teamsRouter);
+app.use('/courts', courtsRouter);
 
-
-// app.use('/courts', courtsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
