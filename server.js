6144aa25cbcd42c79edb209066da5215fd43a425
Hesham <hesham2go@outlook.com>
2021-09-17 10:48:49 -0400

    fix

--- server.js
+++ server.js
@@ -53,10 +53,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
-// app.use('/matches', matchesRouter);
-// app.use('/users', usersRouter);
-// app.use('/teams', teamsRouter);
-// app.use('/courts', courtsRouter);
+app.use('/matches', matchesRouter);
+app.use('/users', usersRouter);
+app.use('/teams', teamsRouter);
+app.use('/courts', courtsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
