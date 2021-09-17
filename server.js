a358414adf61ddd0027005daffa0a5cbd69ce8fc
Hesham <hesham2go@outlook.com>
2021-09-16 23:04:29 -0400

    matches

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 // var courtsRouter = require('./routes/courts');
 // var teamsRouter = require('./routes/teams');
-// var matchesRouter = require('./routes/matches');
+var matchesRouter = require('./routes/matches');
 var app = express();
 
 // view engine setup
@@ -50,9 +50,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
+app.use('/matches', matchesRouter);
 // app.use('/users', usersRouter);
 // app.use('/teams', teamsRouter);
-// app.use('/matches', matchesRouter);
+
 
 // app.use('/courts', courtsRouter);
 // catch 404 and forward to error handler
