e8d503ff1e834dffba997c77799ab83e1954b2d5
Hesham <hesham2go@outlook.com>
2021-09-17 08:04:04 -0400

    err

--- routes/courts.js
+++ routes/courts.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var router = express.Router();
  
-var courtCtrl = require('../controllers/courts')
-router.get('/', courtCtrl.showAll) 
-router.get('/new', courtCtrl.new)
-router.post('/', courtCtrl.create)
+var courtCtrl = require('../controllers/courts');
+
+
+router.get('/', courtCtrl.showAll);
+router.get('/new', courtCtrl.new);
+router.post('/', courtCtrl.create);
 router.delete('/delete/:id', courtCtrl.delete);
 router.get('/edit/:id', courtCtrl.edit);
-router.put('/:id', courtCtrl.update)
+router.put('/:id', courtCtrl.update);
 
 module.exports = router
